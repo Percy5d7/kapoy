91b6953a3a38fa6512f30e76de6a74312fee4c4e
Percy5d7 <percy5d7glen@yahoo.com>
2018-05-13 23:10:27 +0800

    please

---
+++ cachematrix.R
@@ -0,0 +1,15 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- matrix(c(5, 6, 8, 3, 7, 9, 5, 4, 6, 5, 8, 9, 13, 8, 15, 23), nrow=4, ncol = 4, byrow = FALSE, dimnames = NULL) {
+
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- solve(makeCacheMatrix) {
+        ## Return a matrix that is the inverse of 'x'
+}
